a0d080eb1e0b2da8792be0d4bb341ec7cad51b65
Dan Stepanov <danhstepanov@gmail.com>
2013-12-24 02:42:54 -0500

    cleaned up

--- app/controllers/anycdotes_controller.rb
+++ app/controllers/anycdotes_controller.rb
@@ -7,38 +7,34 @@ class AnycdotesController < ApplicationController
 
   def show
   end
+  
   def new
     @anycdote = Anycdote.new
   end
+  
   def edit
   end
 
   def create
     @anycdote = Anycdote.new(anycdote_params)
-
-    respond_to do |format|
       if @anycdote.save
         redirect_to @anycdote, notice: 'Anycdote was successfully created.' 
       else
       render action: 'new'
       end
-    end
   end
 
   def update
-    respond_to do |format|
       if @anycdote.update(anycdote_params)
         redirect_to @anycdote, notice: 'Anycdote was successfully updated.' 
       else
         render action: 'edit' 
       end
-    end
   end
 
   def destroy
     @anycdote.destroy
       redirect_to anycdotes_url 
-    end
   end
 
   private
