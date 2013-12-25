93fceafb08867865cce83d7d9d05efb3e0fbfb50
Dan Stepanov <danhstepanov@gmail.com>
2013-12-24 21:18:46 -0500

    authentication added

--- app/controllers/anycdotes_controller.rb
+++ app/controllers/anycdotes_controller.rb
@@ -1,6 +1,7 @@
 class AnycdotesController < ApplicationController
   before_action :set_anycdote, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @anycdotes = Anycdote.all
   end
@@ -9,14 +10,14 @@ class AnycdotesController < ApplicationController
   end
   
   def new
-    @anycdote = Anycdote.new
+    @anycdote = current_user.anycdotes.build
   end
   
   def edit
   end
 
   def create
-    @anycdote = Anycdote.new(anycdote_params)
+    @anycdote = current_user.anycdotes.build(anycdote_params)
       if @anycdote.save
         redirect_to @anycdote, notice: 'Anycdote was successfully created.' 
       else
@@ -41,6 +42,12 @@ class AnycdotesController < ApplicationController
     def set_anycdote
       @anycdote = Anycdote.find(params[:id])
     end
+
+  def correct_user
+      @anycdote = current_user.anycdotes.find_by(id: params[:id])
+      redirect_to anycdotes_path, notice: "Not authorized to edit this anycdote" if @anycdote.nil?
+  end
+
     def anycdote_params
       params.require(:anycdote).permit(:title, :story, :location,)
     end
