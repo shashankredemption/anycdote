1378e0958631d668f6bd6812495703a9f9b9427f
Dan Stepanov <danhstepanov@gmail.com>
2013-12-24 20:38:15 -0500

    minor editing

--- db/migrate/20131224080308_add_user_id_to_anycdotes.rb
+++ db/migrate/20131224080308_add_user_id_to_anycdotes.rb
@@ -1,7 +1,6 @@
 class AddUserIdToAnycdotes < ActiveRecord::Migration
   def change
-    add_column :anycdotes, :user_id, :string
-    add_column :anycdotes, :integer, :string
-    add_index :anycdotes, :integer
+    add_column :anycdotes, :user_id, :integer
+    add_index :anycdotes, :user_id
   end
 end
